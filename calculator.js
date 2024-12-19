975409ad3ebb5a559fb35198b88a8264963abc26
ssmith2006 <shantel.smith@hotmail.com>
2024-12-19 17:56:11 +0000

    changed variable and alert

--- calculator.js
+++ calculator.js
@@ -1,19 +1,19 @@
 "use sctrict";
 
-let operation = prompt("Enter an operation: +, -, *, / ");
+let operator = prompt("Enter an operation: +, -, *, / ");
 let firstNumber = prompt("Enter the first number");
 firstNumber = Number(firstNumber);
 let secondNumber = prompt("Enter the second number");
 secondNumber = Number(secondNumber);
 
 if (operator === "+") {
-  alert("firstNumber + secondNumber");
+  alert(firstNumber + secondNumber);
 } else if (operator === "-") {
-  alert("firstNumber - secondNumber");
+  alert(firstNumber - secondNumber);
 } else if (operator === "*") {
-  alert("firstNumber * secondNumber");
+  alert(firstNumber * secondNumber);
 } else if (operator === "/") {
-  alert("firstNumber / secondNumber");
+  alert(firstNumber / secondNumber);
 } else {
   alert("invalid");
 }
